23047cb2b41d290ce18b16e610c4b7999ae85090
lihang <18632643738@qq.com>
2022-04-17 13:28:07 +0800

    hot-fix commit1

--- src/main/java/com/systop/GitTest.java
+++ src/main/java/com/systop/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello, git2");
         System.out.println("hello, git3");
         System.out.println("这是 hot-fix 分支");
+        System.out.println("这是 master 合并冲突");
     }
 }
